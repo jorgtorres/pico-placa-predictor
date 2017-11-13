4994a2963ab82ac1e69bfccfc407b00c25e5deef
Jorge Torres <jorgevtorresr@gmail.com>
2017-11-12 23:00:45 -0500

    Pico y Placa Review

--- app/picoyplaca.py
+++ app/picoyplaca.py
@@ -8,10 +8,11 @@ class PicoYPlaca():
         self.time = time
 
     def CanItBeOnRoad(self):
+        moment = datetime.datetime.strptime("{0} {1}".format(self.date, self.time), '%Y-%m-%d %H:%M')
         last_digit = int(self.getLicense().strip()[-1])
-        day = self.getMoment().weekday()
-        if (self.getMoment().time() >= datetime.time(7, 0) and self.getMoment().time() <= datetime.time(9, 30)) \
-        or (self.getMoment().time() >= datetime.time(16, 0) and self.getMoment().time() <= datetime.time(19, 30)):
+        day = moment.weekday()
+        if (moment.time() >= datetime.time(7, 0) and moment.time() <= datetime.time(9, 30)) \
+        or (moment.time() >= datetime.time(16, 0) and moment.time() <= datetime.time(19, 30)):
             if (day == 0 and (last_digit == 1 or last_digit == 2)) or (day == 1 and (last_digit == 3 or last_digit == 4)) \
             or (day == 2 and (last_digit == 5 or last_digit == 6)) or (day == 3 and (last_digit == 7 or last_digit == 8)) \
             or (day == 4 and (last_digit == 9 or last_digit == 0)):
@@ -20,7 +21,3 @@ class PicoYPlaca():
 
     def getLicense(self):
         return self.license
-
-    def getMoment(self):   
-        moment = datetime.datetime.strptime("{0} {1}".format(self.date, self.time), '%Y-%m-%d %H:%M')
-        return moment
